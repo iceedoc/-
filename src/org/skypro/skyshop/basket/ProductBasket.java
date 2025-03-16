832997bb7d53d8b32bbea61932df322e6f55539b
User <paashtetik@gmail.com>
2025-03-16 23:45:06 +0300

    first job2

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,61 +1,28 @@
 package org.skypro.skyshop.basket;
 
-
 import org.skypro.skyshop.product.Product;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductBasket {
-
-    private final Product[] products = new Product[6];
-    private int productCount = 0;
+    private final List<Product> products = new ArrayList<>();
 
     public void addProduct(Product product) {
-        if (productCount < products.length) {
-            products[productCount] = product;
-            productCount++;
-            System.out.println("Продукт " + product.getName() + " добавлен в корзину.");
-        } else {
-            System.out.println("Невозможно добавить продукт.");
-        }
+        products.add(product);
     }
 
-    public int getTotalPrice() {
-        int totalPrice = 0;
-        for (Product product : products) {
-            if (product != null) {
-                totalPrice += product.getPrice();
-            }
-        }
-        return totalPrice;
-    }
+    public void printContents() {
+        int total = 0;
+        int specialCount = 0;
 
-    public void printBasket() {
-        if (productCount == 0) {
-            System.out.println("В корзине пусто.");
-            return;
+        for (Product p : products) {
+            System.out.println(p);
+            total += p.getPrice();
+            if (p.isSpecial()) specialCount++;
         }
 
-        for (Product product : products) {
-            if (product != null) {
-                System.out.println(product.getName() + ": " + product.getPrice());
-            }
-        }
-        System.out.println("Итого: " + getTotalPrice());
-    }
-
-    public boolean containsProduct(String name) {
-        for (Product product : products) {
-            if (product != null && product.getName().equals(name)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void clearBasket() {
-        Arrays.fill(products, null);
-        productCount = 0;
-        System.out.println("Корзина очищена.");
+        System.out.printf("Итого: %d%n", total);
+        System.out.printf("Специальных товаров: %d%n", specialCount);
     }
 }
